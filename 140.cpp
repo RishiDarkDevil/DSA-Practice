f85cb31f310e7b3e0528bb1d41a709609ede1c3d
RishiDarkDevil <rishi8001100192@gmail.com>
2022-10-20 15:06:08 +0530

    Wells Fargo Error Code sole

--- 140.cpp
+++ 140.cpp
@@ -1,33 +1,45 @@
+// https://leetcode.com/discuss/interview-question/889539/wells-fargo
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int findProjects(priority_queue<int> &scores, int p, int q) {
-	int val_spent = 0, ans = 0;
-	int top = scores.top();
-	while (top >= val_spent) {
-		top = top - p + q;
-		scores.pop();
-		scores.push(top);
-		top = scores.top();
-		val_spent += q;
-		ans++;
-	}
-	return ans;
-
+bool isProjEnough(vector<int> &scores, int p, int q, int x) {
+	int num_projs = 0;
+	for (auto &score: scores)
+		num_projs += max(0, (int)ceil(score - q*x / ((p-q)*1.0)));
+	return num_projs <= x;
 }
 
 int main() {
+	// number of employees
 	int n;
 	cin >> n;
 
-	priority_queue<int> scores;
+	// scores of the employees
+	vector<int> scores; int max_score = 0;
 	for (int i = 0; i < n; ++i) {
-		int x; cin >> x; scores.push(x);
+		int x; 
+		cin >> x; 
+		scores.push_back(x);
+		max_score = max(max_score, x);
 	}
 
+	// P and Q
 	int p, q;
 	cin >> p >> q;
 
-	cout << findProjects(scores, p, q);
-
+	// Binary Search the Solution between 0 and max_score 
+	// cause the atleast as many projects will be needed so as to finish the max_score 
+	int lb = 0, ub = max_score;
+	while (lb < ub) {
+		int mid = (lb+ub) / 2;
+		if (isProjEnough(scores, p, q, mid))
+			ub = mid;
+		else
+			lb = mid+1;
+	}
+	if (isProjEnough(scores, p, q, ub))
+		cout << ub;
+	else
+		cout << lb;
 }
